6b68e085c0d7ca71708dff9983a953df884c09f0
Nafisa <Nafisafaruna@gmail.com>
2023-03-26 12:57:33 +0100

    initialize

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,18 @@
+const base = require("./webpack.base.config")
+const { merge } = require('webpack-merge');
+
+const webpack = require("webpack")
+const config = require('.././config/dev.config')
+
+module.exports = merge(base, {
+    // This contains additional production settings
+    plugins: [
+        new webpack.DefinePlugin({
+            "process": {
+                env: config
+            }
+        })
+    ],
+   
+    
+})
