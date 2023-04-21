a2f970bb300f9745a1fdaebcd5bf16320e75f048
Gabbygabijo <gabrielfashedemi@gmail.com>
2023-04-21 17:02:59 +0100

    Initial commit

---
+++ Client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        tblue: '#5E7CE8',
+        tgreen: '#27AE60',
+        tblack: '#111111',
+        twhite: '#F2F2F2',
+      },
+    },
+  },
+  plugins: [],
+}
+
