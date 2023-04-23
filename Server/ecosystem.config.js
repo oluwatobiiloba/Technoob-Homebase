f7be724c6fcbbe8474e09d346e02f4dafe7123dd
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-04-23 03:18:29 +0100

    update instances

--- Server/ecosystem.config.js
+++ Server/ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       NODE_ENV: "production",
     },
     watch: true,
-    instances: 3,
+    instances: "max",
     exec_mode: "cluster",
     ignore_watch: ["node_modules"],
 
