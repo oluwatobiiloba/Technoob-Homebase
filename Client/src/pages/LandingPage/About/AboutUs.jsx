9a070a9bb8fa736e5074f55d3151add7d2b8af00
Gabbygabijo <gabrielfashedemi@gmail.com>
2023-04-28 22:00:28 +0100

    Just a little change

--- Client/src/pages/LandingPage/About/AboutUs.jsx
+++ Client/src/pages/LandingPage/About/AboutUs.jsx
@@ -1,4 +1,3 @@
-import Navbar from './components/Navbar'
 import Header from './components/Header'
 import Section from './components/Section'
 import Footer from './components/Footer'
@@ -6,7 +5,6 @@ import Footer from './components/Footer'
 var AboutUs = () => {
     return (
         <div>
-            <Navbar/>
             <Header/>
             <Section/>
             <Footer/>
