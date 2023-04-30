85a851a8716add759d3c437b6ec75cffb1cac01d
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-04-30 08:00:59 +0100

    add permission

--- Server/routes/resources.js
+++ Server/routes/resources.js
@@ -7,7 +7,7 @@ const middleware = require('../middleware/index');
 router.get('/all', resources.get_all)
 router.post('/create', resources.create)
 router.get('/count', resources.count)
-router.post('/delete/:id', middleware.auth.isAuthenticated, middleware.auth.hasPermission,resources.remove)
+router.post('/delete/:id', middleware.auth.isAuthenticated, middleware.auth.hasPermission('admin:RemoveResource'),resources.remove)
 router.post('/rate/:id', middleware.auth.isAuthenticated , resources.rate)
 
 module.exports = router;
