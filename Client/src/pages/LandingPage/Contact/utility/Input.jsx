f46b271e69dbbe8d84f5769d34676f02b23d966d
Gabbygabijo <gabrielfashedemi@gmail.com>
2023-05-02 17:27:00 +0100

    dynamic allocation

--- Client/src/pages/LandingPage/Contact/utility/Input.jsx
+++ Client/src/pages/LandingPage/Contact/utility/Input.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Input = ({ name }) => {
+const Input = ({ name, value, onChange }) => {
   return (
     <>
     <label htmlFor="fullname" className=' text-2xl font-semibold py-10 px-4 ' >{name}</label><br/>
-    <input type="text" placeholder={name} className=' w-[100%] rounded-xl m-1 border px-10 py-4 my-10 outline-0 ring-1 bg-white' />
+    <input type="text" value={value} onChange={onChange} placeholder={name} className=' w-[100%] rounded-xl m-1 border px-10 py-4 my-10 outline-0 ring-1 bg-white' />
     </>
   )
 }
