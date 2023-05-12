f7adc9a2e8047cee9f9de501659e686529ca1a52
Gabbygabijo <gabrielfashedemi@gmail.com>
2023-05-12 22:25:15 +0100

    new correction

--- Client/src/pages/LandingPage/About/components/Section.jsx
+++ Client/src/pages/LandingPage/About/components/Section.jsx
@@ -9,7 +9,7 @@ const Section = () => {
       <SectionOne/>
       <SectionTwo/>
       <SectionThree/>
-      <div className='flex justify-end px-5 md:text-5xl nun'>
+      <div className='flex justify-end px-5 md:text-5xl underline nun'>
         Meet the whole team <img src={img} alt="" className=' h-3 w-3 mt-[8px] md:h-auto md:w-auto '/>
       </div>
     </>
