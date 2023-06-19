46e0a08d4ce110dcc8459fb47e3046b164271903
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-06-19 12:56:30 +0100

    fix bug

--- Server/app.js
+++ Server/app.js
@@ -32,7 +32,7 @@ app.use(cors({
   methods: 'GET,PUT,POST,DELETE',
   allowedHeaders: 'Content-Type',
   credentials: true,
-  exposedHeaders: 'Set-Cookie','Date'
+  exposedHeaders: ['Set-Cookie', 'Date']
 }));
 
 const httpRequestDurationMicroseconds = new prometheus.Histogram({
